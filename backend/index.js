8773a3f9b6b6ceea648ad28383b8a3ac1039f60a
Elena <lenshik_1999@mail.ru>
2025-04-22 17:04:27 +1000

    add const, func start server, cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,36 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
 
+dotenv.config()
 const app = express()
 
-app.listen(5000, () => {
-    console.log("server started")
-})
+
+/*************importantConst*************/
+const PORT = process.env.PORT
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+/****************************************/
+
+
+
+/*************middleware*************/
+app.use(cors())
+/***********************************/
+
+
+
+async function startServer() {
+    try {
+        await mongoose.connect(
+            `mongodb://${DB_USER}:${DB_PASSWORD}@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&authSource=${DB_NAME}&appName=mongosh+2.5.0`
+        )
+
+       app.listen(PORT, () => console.log("server started")) 
+    } catch (error) {
+        console.log("error")
+    }
+}
+startServer()
