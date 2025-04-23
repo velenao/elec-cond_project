6b349c05e5cec41c9ec14072b639e9680905e46b
Elena <lenshik_1999@mail.ru>
2025-04-23 20:45:29 +1000

    added starting react

---
+++ frontend/src/pages/electricPages/electricPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const electricPage = () => {
+  return <div>electricPage</div>
+}
---
+++ frontend/src/pages/mainPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const mainPage = () => {
+    return <div>mainPage</div>
+}
