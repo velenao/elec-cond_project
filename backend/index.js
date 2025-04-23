a682ccc1babc240b63a842a53c86d5b76aed21a1
Elena <lenshik_1999@mail.ru>
2025-04-23 18:03:55 +1000

    added starting react

--- backend/index.js
+++ backend/index.js
@@ -18,8 +18,8 @@ const DB_NAME = process.env.DB_NAME
 
 /*************middleware*************/
 app.use(cors())
+app.use(express.json())
 /***********************************/
-
 
 
 async function startServer() {
