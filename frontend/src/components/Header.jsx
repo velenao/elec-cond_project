93dcee83aa45625d57abc4ff5fcdacde9b4626f1
Elena <lenshik_1999@mail.ru>
2025-04-25 17:15:34 +1000

    correcting headers for dif pages

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import { useLocation } from "react-router";
 
 export const Header = () => {
+  const location = useLocation();
+
   return (
     <React.Fragment>
       <div className="header">
         <div className="logo"> Header </div>
         <ul className="nav">
-          <li className="nav_item">о нас</li>
-          <li className="nav_item">услуги</li>
-          <li className="nav_item">контакты</li>
-          <li className="nav_item">работы</li>
-          <li className="nav_item">прайс</li>
-          <li className="nav_item">эл/конд</li>
+          <li>
+            <a href="1">о нас</a>
+          </li>
+          <li>
+            <a href="1">услуги</a>
+          </li>
+          <li>
+            <a href="1">контакты</a>
+          </li>
+          <li>
+            <a href="1">работы</a>
+          </li>
+          <li>
+            <a href="1">прайс</a>
+          </li>
+          {location.pathname === "/electric" && (
+            <div>
+              <li>
+                <a href="1">эл</a>
+              </li>
+            </div>
+          )}
+          {location.pathname === "/conditioners" && (
+            <li>
+              <a href="1">конд</a>
+            </li>
+          )}
         </ul>
       </div>
     </React.Fragment>
