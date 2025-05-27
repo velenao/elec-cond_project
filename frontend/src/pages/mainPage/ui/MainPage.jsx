e7ddceba60e6bcd7727eae22893e974f4e32c5c5
Elena <lenshik_1999@mail.ru>
2025-05-27 17:56:21 +1000

    add fold assets & del blur & command play front+back

--- frontend/src/pages/mainPage/ui/MainPage.jsx
+++ frontend/src/pages/mainPage/ui/MainPage.jsx
@@ -1,8 +1,10 @@
 import React, { useRef } from "react";
 import './index.css'
-import videoCond from './cond.mp4'
-import videoElec from './elecVideo.mp4'
 import usePlayVideo from './playVideo'
+import videoCond from '../../../assets/conditionersAssets/condVideo/cond.mp4'
+// import videoElec from './'
+import elecImg from '../../../assets/electricAssets/elecImg/elec.jpg'
+import condImg from '../../../assets/conditionersAssets/condImg/cond.png'
 
 
 
@@ -18,16 +20,14 @@ export const MainPage = () => {
       <div className="main_page">
         <div className="main_title">Профессиональный монтаж и сервисное обслуживание кондиционеров и электрики</div>
         <div className="main_link">
-        <div className="link">
-            <a href="/electric">
-              <video className="main_video" id="video_electric" ref={vidRefElec} src={videoElec} loop muted></video>
+            <a className="link" href="/electric">
+              <video className="main_video" id="video_electric" ref={vidRefElec} loop muted></video>
+              <img src={elecImg} alt="элетрика" className="img_video"/>
             </a>
-          </div>
-          <div className="link">
-            <a href="/conditioners">
+            <a className="link" href="/conditioners">
               <video className="main_video" id="video_conditioners" ref={vidRefCond} src={videoCond} loop muted></video>
+              <img src={condImg} alt="кондиционеры" className="img_video"/>
             </a>
-          </div>
         </div>
       </div>
     </>
